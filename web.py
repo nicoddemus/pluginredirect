67fc9854f166cc5bc3b1081ec887bd15112b9682
Bruno Oliveira <nicoddemus@gmail.com>
2015-03-04 07:44:05 -0300

    Removed auto-redirect
    I don't want to maintain this project forever, and so making people
    actually click on the link is a good way for them to notice the
    project has moved and update their bookmarks.

--- web.py
+++ web.py
@@ -12,16 +12,11 @@ def index():
 <!DOCTYPE HTML>
 <html lang="en-US">
     <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="refresh" content="5000;url=http://plugincompat.herokuapp.com">
-        <script type="text/javascript">
-            window.location.href = "http://plugincompat.herokuapp.com"
-        </script>
         <title>Page Redirection</title>
     </head>
     <body>
         <!-- Note: don't tell people to `click` the link, just tell them that it is a link. -->
-        If you are not redirected automatically, follow the <a href='http://plugincompat.herokuapp.com'>link to plugincompat page</a>.
+        This page has moved, please follow the link to <a href='http://plugincompat.herokuapp.com'>plugincompat</a> page.
     </body>
 </html>
     """
